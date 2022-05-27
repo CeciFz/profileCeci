4e9335ac1b6a97a455b005ae2283a2df78970339
Ceci <ceci.fz87@gmail.com>
2022-05-27 10:13:14 -0300

    Se agrega que se cierre la barra lateral al clickear fuera de ella

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
-let menu = document.getElementById('navbar');
-let burguer = document.getElementById('menu--burguer');
-
-burguer.addEventListener('click', function(){
-    menu.classList.toggle("show-menu");
-})
-
-
-
 window.addEventListener("scroll" , function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky" , window .scrollY > 0)
 })
 
 
+const menu = document.getElementById('navbar');
+const burguer = document.getElementById('burguer');
+
+
+burguer.addEventListener('click', ()=>{
+        menu.classList.toggle('show-menu');
+})
+
+window.addEventListener('click', e =>{
+    if( menu.classList.contains('show-menu') && e.target != burguer && e.target !=menu ){
+        menu.classList.remove('show-menu');
+    }
+})
 
 
